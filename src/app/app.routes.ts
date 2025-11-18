c1070d438bc750b5486f3f62ffab5d20366ddd32
Akhil Mannam <am3646@njit.edu>
2025-11-18 22:52:50 +0000

    Milestone2:pt1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
+import { RegisterComponent } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  onLogin() {
+    alert("Login works!");
+  }
+}
+
